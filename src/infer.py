aa9ae925e86e8cc4efa8ff6a6a6274c8871123c7
Bahushruth <bahushruth.cs@quillbot.com>
2023-01-07 20:52:51 +0530

    changed infer.py

--- src/infer.py
+++ src/infer.py
@@ -7,9 +7,20 @@ tokenizer = AutoTokenizer.from_pretrained(model_identifier)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_identifier)
 
 def get_summary(input_text):
-    input_ids = tokenizer.encode(input_text, return_tensors="pt")
-    tok_len = len(input_ids[0])
-    outputs = model.generate(input_ids, max_new_tokens=int(tok_len), min_length=int(tok_len/2))
-    decoded = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    input_ids = tokenizer.encode(
+        "summarize: " + input_text, return_tensors="pt", add_special_tokens=True
+    )
 
-    return decoded
+    generated_ids = model.generate(
+        input_ids=input_ids,
+        num_beams=5,
+        max_length=512,
+        repetition_penalty=2.5,
+        length_penalty=1,
+        early_stopping=True,
+        num_return_sequences=1,
+    )
+
+    summary = tokenizer.decode(generated_ids[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
+
+    return summary
